e444ebc7a728af84deb987bf09453a43faf90ab9
anujashingne <anuja254@gmail.com>
2017-07-08 23:41:57 +0530

    Committing node js files

---
+++ js/mongoConnector.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/newDB');
+
+var Users = mongoose.model('Users', { id: Number, name: String });
+
+var user1 = new Users({ id:'123' , name: 'Prinstine' });
+
+user1.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
